54cedb28182f249d4fa4c59c66cac2cde44cae83
Guntis Smaukstelis <gsmaukstelis@evolution.com>
2023-05-18 16:39:14 +0300

    Indicate in web title that working in dev environment

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
+import VitePluginHtmlEnv from 'vite-plugin-html-env'
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), VitePluginHtmlEnv()],
   server: {
     port: 3000,
   },
