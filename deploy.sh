80a192a401eeb010f2ba9705ef611938a38071f8
Guntis Smaukstelis <gsmaukstelis@evolution.com>
2024-01-21 22:30:23 +0200

    Update fly.io deployment script with env variables

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# build web files
 cd web
 npm run build
 cd ..
 
+# Check and set secrets
+if [ -s .env-fly ]; then
+  while read line; do
+    echo "Setting secrets: $line"
+    fly secrets set "$line"
+  done < .env-fly
+else
+  echo ".env-fly file is missing or empty, skipping setting secrets"
+  exit 1
+fi
+
+# Test, build and deply
 if sbt test; then
   sbt assembly && fly deploy
 else
   echo "Tests failed, skipping deploy!"
+  exit 1
 fi
