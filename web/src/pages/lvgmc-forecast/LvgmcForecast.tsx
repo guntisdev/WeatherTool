a5aeddd351dbdf19cb030b8447005be481191e97
Guntis Smaukstelis <gsmaukstelis@evolution.com>
2025-03-15 22:51:59 +0200

    Toggle between evening and morning forecasts

--- web/src/pages/lvgmc-forecast/LvgmcForecast.tsx
+++ web/src/pages/lvgmc-forecast/LvgmcForecast.tsx
@@ -15,7 +15,10 @@ import styles from './lvgmc.module.css'
 //  Latvija_LTV_pilsetas_tekosa_dn.csv
 //  Latvija_faktiskais_laiks.csv
 
+type Section = 'evening' | 'morning'
+
 export const LvgmcForecast: Component<{}> = () => {
+    const [getActiveSection, setActiveSection] = createSignal<Section>('evening')
     const [getEveningLines, setEveningLines] = createSignal<string[]>([])
     const [getMorningLines, setMorningLines] = createSignal<string[]>([])
 
@@ -30,34 +33,49 @@ export const LvgmcForecast: Component<{}> = () => {
     )
 
     return <>
-        <p>Vakara ēters</p>
-
-        <div class={styles.twoCanvas}>
-            <TableTwo getCsvLines={() => getEveningLines().slice(60, 67)} />
-            <TableTwo getCsvLines={() => getEveningLines().slice(69, 76)} />
+        <input
+            type='button'
+            value='Vakara ēters'
+            onClick={()=>setActiveSection('evening')}
+            class={getActiveSection() === 'evening' ? styles.activeButton : styles.button}
+        />
+        <input
+            type='button'
+            value='Rīta ēters'
+            onClick={()=>setActiveSection('morning')}
+            class={getActiveSection() === 'morning' ? styles.activeButton : styles.button}
+        />
+        <br/><br/>
+        
+        <div style={{ display: getActiveSection() === 'evening' ? 'block' : 'none' }}>
+            <CsvMapData
+                getCityLines={() => getEveningLines().slice(4, 22)}
+                getWindLine={() => getEveningLines()[26]}
+                bgImgUrl={nightBgMap}
+            />
+            <CsvMapData
+                getCityLines={() => getEveningLines().slice(31, 49)}
+                getWindLine={() => getEveningLines()[57]}
+                bgImgUrl={dayBgMap}
+            />
+            <TableFour getCsvLines={() => getEveningLines()} />
+            <div class={styles.twoCanvas}>
+                <TableTwo getCsvLines={() => getEveningLines().slice(60, 67)} />
+                <TableTwo getCsvLines={() => getEveningLines().slice(69, 76)} />
+            </div>
         </div>
-        <CsvMapData
-            getCityLines={() => getEveningLines().slice(4, 22)}
-            getWindLine={() => getEveningLines()[26]}
-            bgImgUrl={nightBgMap}
-        />
-        <CsvMapData
-            getCityLines={() => getEveningLines().slice(31, 49)}
-            getWindLine={() => getEveningLines()[57]}
-            bgImgUrl={dayBgMap}
-        />
-        <TableFour getCsvLines={() => getEveningLines()} />
 
-        <p>Rīta ēters</p>
-        <CsvMapData
-            getCityLines={() => getMorningLines().slice(4, 22)}
-            getWindLine={() => getMorningLines()[23]}
-            bgImgUrl={dayBgMap}
-        />
-        <TableFour getCsvLines={() => getMorningLines()} />
-        <div class={styles.twoCanvas}>
-            <TableTwo getCsvLines={() => getMorningLines().slice(33, 40)} />
-            <TableTwo getCsvLines={() => getMorningLines().slice(42, 49)} />
+        <div style={{ display: getActiveSection() === 'morning' ? 'block' : 'none' }}>
+            <CsvMapData
+                getCityLines={() => getMorningLines().slice(4, 22)}
+                getWindLine={() => getMorningLines()[23]}
+                bgImgUrl={dayBgMap}
+            />
+            <TableFour getCsvLines={() => getMorningLines()} />
+            <div class={styles.twoCanvas}>
+                <TableTwo getCsvLines={() => getMorningLines().slice(33, 40)} />
+                <TableTwo getCsvLines={() => getMorningLines().slice(42, 49)} />
+            </div>
         </div>
     </>
 }
